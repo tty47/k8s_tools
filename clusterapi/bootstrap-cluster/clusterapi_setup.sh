92104f789dcc3cf0b44b359c89fdae170c6bfe1c
Jose Ramon Mañes <jr.mb47@gmail.com>
2022-12-07 18:39:54 +0100

    Add some validations

--- clusterapi/bootstrap-cluster/clusterapi_setup.sh
+++ clusterapi/bootstrap-cluster/clusterapi_setup.sh
@@ -1,23 +1,23 @@
-#!/bin/sh
-# Source: https://kind.sigs.k8s.io/docs/user/local-registry/
-clear
+#!/bin/zsh
+# ============================================================================
+#Colours
+greenColour="\e[0;32m\033[1m"
+endColour="\033[0m\e[0m"
+blueColour="\e[0;34m\033[1m"
+grayColour="\e[0;37m\033[1m"
 
 KUBERNETES_VERSION="v1.25.3"
 KCP_NODES="3"
 KWM_NODES="3"
+CILIUM_VERSION="1.12.4"
 
+# ============================================================================
+clear
 set -o errexit
 
+# ============================================================================
+# Default path to the kubeconfig
 export KUBECONFIG=~/.kube/config
-
-echo "========================================= >"
-echo "Initialize the management cluster..."
-# Enable the experimental Cluster topology feature.
-export CLUSTER_TOPOLOGY=true
-# Initialize the management cluster
-clusterctl init --infrastructure docker
-echo "========================================= >"
-
 # The list of service CIDR, default ["10.128.0.0/12"]
 export SERVICE_CIDR=["10.96.0.0/12"]
 # The list of pod CIDR, default ["192.168.0.0/16"]
@@ -28,16 +28,45 @@ export SERVICE_DOMAIN="k8s.test"
 #enabled Pod Security Standard:
 export ENABLE_POD_SECURITY_STANDARD="false"
 
-echo "What's the name of your cluster? \n"
-read clusterName
-echo "Cluster name is: [${clusterName}]"
-echo "Cluster version is: [${KUBERNETES_VERSION}]"
-echo "========================================= >"
+# ============================================================================
+# line reuse line code
+function line(){
+  echo -e "${grayColour}============================================================= > ${endColour}"
+}
 
-echo "How many Control Plane Nodes do you want? \n"
+# ============================================================================
+line 
+echo "Initialize the management cluster..."
+# Enable the experimental Cluster topology feature.
+export CLUSTER_TOPOLOGY=true
+# Initialize the management cluster
+IS_INIT=$(kubectl get po -n cert-manager -o name -l app=cert-manager)
+if [[ -z "${IS_INIT}" ]]; then
+  echo "Setting up Management cluster..."
+  clusterctl init --infrastructure docker
+else
+  line
+  echo "Management Cluster components already installed"
+fi
+line
+
+# ============================================================================
+echo -e "${blueColour}What's the name of your cluster?${endColour}"
+read clusterName
+if [[ -z "${clusterName}" ]]; then
+  echo "ERROR: You have to specify the cluster name.."
+  exit 1
+fi
+echo -e "Cluster name is: ${greenColour}[${clusterName}]${endColour}"
+echo -e "Cluster version is: ${greenColour}[${KUBERNETES_VERSION}]${endColour}"
+line
+
+echo "How many Control Plane Nodes do you want?"
 read KCP_NODES
-echo "How many Worker Machine Nodes do you want? \n"
+line
+echo "How many Worker Machine Nodes do you want?"
 read KWM_NODES
+line
 
 if [[ -z "${KCP_NODES}" ]] || [[ -z "${KCP_NODES}" ]]; then
   echo "ERROR: You must set the values for the Control Plane nodes and Worker
@@ -55,12 +84,12 @@ clusterctl generate cluster ${clusterName} --flavor development \
   > capi-${clusterName}.yaml
 
 sed -i -e "s/quick-start/${clusterName}/g" capi-${clusterName}.yaml
-rm capi-${clusterName}.yaml-e
+rm -f capi-${clusterName}.yaml-e
 
-echo "========================================= >"
+line
 echo "Check the file: [./capi-${clusterName}.yaml]"
 
-echo "========================================= >"
+line
 echo "Applying the manifest..."
 kubectl apply -f ./capi-${clusterName}.yaml
 
@@ -69,42 +98,47 @@ kubectl get cluster
 sleep 10
 
 #https://cluster-api.sigs.k8s.io/clusterctl/developers.html#additional-notes-for-the-docker-provider
-echo "========================================= >"
+line
 echo "Describe the cluster [${clusterName}]"
 clusterctl describe cluster ${clusterName}
 
-echo "========================================= >"
-echo "Get the controlPlane [${clusterName}]"
+line
+echo -e "${blueColour}Get the ControlPlane [${clusterName}] ${endColour}"
 kubectl get kubeadmcontrolplane
 
-echo "========================================= >"
-echo "Get the kubeconfig [${clusterName}]"
+line
+echo -e "${blueColour}Get the KubeConfig [${clusterName}] ${endColour}"
 clusterctl get kubeconfig  ${clusterName} > ./capi-${clusterName}.kubeconfig
 
 # source: https://cluster-api.sigs.k8s.io/clusterctl/developers.html#fix-kubeconfig-when-using-docker-desktop-and-clusterctl
 sed -i -e "s/server:.*/server: https:\/\/$(docker port ${clusterName}-lb 6443/tcp | sed "s/0.0.0.0/127.0.0.1/")/g" ./capi-${clusterName}.kubeconfig
-rm capi-${clusterName}.kubeconfig-e
+rm -f capi-${clusterName}.kubeconfig-e
 
-echo "========================================= >"
-echo "Check the nodes using the new kubeconfig [./capi-${clusterName}.kubeconfig]"
+line
+echo -e "Check the nodes using the new kubeconfig ${blueColour}[./capi-${clusterName}.kubeconfig]${endColour}"
 kubectl get no --kubeconfig=./capi-${clusterName}.kubeconfig
 export KUBECONFIG=./capi-${clusterName}.kubeconfig
 
-echo "========================================= >"
-echo "Do you want to install Cilium as CNI?, y/n \n"
+# ============================================================================
+line
+echo "Do you want to install Cilium as CNI?, y/n"
 read installCilium 
-
-if [ "$installCilium" == "y" ];then
+if [[ -z "${installCilium}" ]]; then
+  echo "ERROR: You must set the value to install CNI..."
+  exit 1
+fi
+if [[ "${installCilium}" == "y" ]];then
   echo "Yes, install it"
   helm repo add cilium https://helm.cilium.io/
-  helm install cilium cilium/cilium --version 1.12.4 \
-  --namespace kube-system
+  helm install cilium cilium/cilium \
+    --version ${CILIUM_VERSION} \
+    --namespace kube-system
 else 
   echo "NO thanks!"
 fi
 
-echo "========================================= >"
+line
 echo "Checking nodes & pods.."
 kubectl get no
 kubectl get po -A
-echo "========================================= >"
+line
