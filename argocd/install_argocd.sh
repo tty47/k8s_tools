42ee57b4a659b8b9e8bc5979cb15b0de3ee2e7ac
Jose Ramon Mañes <jr.mb47@gmail.com>
2022-12-16 23:53:17 +0100

    Add install app of apps

--- argocd/install_argocd.sh
+++ argocd/install_argocd.sh
@@ -7,4 +7,8 @@ echo "Credentials:
 username: admin
 password: $(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo)"
 
+
+echo "Install ArgoCD App of Apps"
+kubectl apply -f repository_k8s_tools.yaml
+
 kubectl port-forward svc/argocd-server -n argocd 8080:443
