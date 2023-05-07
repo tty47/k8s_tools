44e115687700a3eff0a14ca7fb310d251d662943
Jose Ramon Mañes Blasco <jr.mb47@gmail.com>
2023-05-07 15:59:49 +0200

    feat: add alias

--- cks/scripts/install_worker.sh
+++ cks/scripts/install_worker.sh
@@ -17,11 +17,10 @@ echo 'set expandtab' >> ~/.vimrc
 echo 'source <(kubectl completion bash)' >> ~/.bashrc
 echo 'alias k=kubectl' >> ~/.bashrc
 echo 'alias c=clear' >> ~/.bashrc
-echo 'alias l="ls -ltra"' >> ~/.bashrc
+echo 'alias l="ls"' >> ~/.bashrc
 echo 'alias ll="ls -ltra"' >> ~/.bashrc
 echo 'complete -F __start_kubectl k' >> ~/.bashrc
 sed -i '1s/^/force_color_prompt=yes\n/' ~/.bashrc
-
 
 ### disable linux swap and remove any existing swap partitions
 swapoff -a
